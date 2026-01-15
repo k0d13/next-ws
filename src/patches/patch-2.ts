e893fb2de59a61a98e2aa53c903e5a34904bf46b
Kodie <apteryx.xyz@gmail.com>
2026-01-15 16:54:50 +1300

    Bump patch supported range to 16.1.2 (#165)
    Co-authored-by: apteryxxyz <apteryxxyz@users.noreply.github.com>

--- src/patches/patch-2.ts
+++ src/patches/patch-2.ts
@@ -21,7 +21,7 @@ export const patchCookies = definePatchStep({
 
 export default definePatch({
   name: 'patch-2',
-  versions: '>=15.0.0 <=16.1.1',
+  versions: '>=15.0.0 <=16.1.2',
   steps: [
     p1_patchNextNodeServer,
     p1_patchRouterServer,
