674dc9d2f4b27d5d1a0b3fdca1d95cdb647928a6
apteryxxyz <apteryxxyz@users.noreply.github.com>
2026-04-16 06:37:47 +0000

    Bump patch supported range to 16.2.4

--- src/patches/patch-2.ts
+++ src/patches/patch-2.ts
@@ -21,7 +21,7 @@ export const patchCookies = definePatchStep({
 
 export default definePatch({
   name: 'patch-2',
-  versions: '>=15.0.0 <=16.2.3',
+  versions: '>=15.0.0 <=16.2.4',
   steps: [
     p1_patchNextNodeServer,
     p1_patchRouterServer,
